a01acdfdf20696f04ee06e8367f8b2bfa97b05e4
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-10 18:55:34 -0400

    2 of 4 tests passing in last function

--- functions.js
+++ functions.js
@@ -1,29 +1,41 @@
 const identity = function (value) {
-  return value
+	return value;
 };
+
 const first = function (array, n) {
-  let newArr=[]
-  if (n === undefined){
-    return array[0]
-  }else {
-  for (let i = 0;  i < n; i++){
-    newArr.push(array[i])
-  }
-  return newArr
-}
+	let newArr = [];
+	if (n === undefined) {
+		return array[0];
+	} else {
+		for (let i = 0; i < n; i++) {
+			newArr.push(array[i]);
+		}
+		return newArr;
+	}
 };
 
-const last = function (array, n) {};
+const last = function (array, n) {
+  let newArr = [];
+	if (n === undefined) {
+		return array[array.length-1];
+	}
+  if (n === 0){
+    return []
+  }
 
-const each = function (collection, callback) {};
+};
+
+const each = function (collection, callback) {
+
+};
 const indexOf = function (array, target) {};
 const map = function (collection, iterator) {};
 
 module.exports = {
-  identity,
-  first,
-  last,
-  each,
-  indexOf,
-  map,
+	identity,
+	first,
+	last,
+	each,
+	indexOf,
+	map,
 };
